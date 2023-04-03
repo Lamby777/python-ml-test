ea7359d8d28328e030886f046a8e02e4a8975ef1
Dex <sheepareawesome777@gmail.com>
2023-04-03 01:01:02 -0400

    Follow next page

--- kaggle-melb/pandas-test.py
+++ kaggle-melb/pandas-test.py
@@ -2,7 +2,16 @@ import pandas as pd
 
 DATA_FILE_PATH = './melb_data.csv'
 
-data = pd.read_csv(DATA_FILE_PATH) 
-out = data.describe()
+features = [
+    "Rooms",
+    "Bathroom",
+    "Landsize",
+    "Lattitude",
+    "Longtitude", # [sic.]
+]
 
-print(out)
+data = pd.read_csv(DATA_FILE_PATH).dropna(axis=0)
+x = data[features]
+y = data.Price
+
+print(x.head())
